edbeff0d00f9e9040c63a958af3cce376381b273
shobhan dash <28.11.shobhan.dash@gmail.com>
2023-09-19 13:21:04 +0530

    elliptical orbits [BUGS]

--- SmartOrbitSatellite.py
+++ SmartOrbitSatellite.py
@@ -1,14 +1,38 @@
-class SmartOrbitSatellite:
-    def __init__(self, name, inclination, semi_major_axis, semi_minor_axis, eccentricity):
+from ursina import *
+import math
+import random
+import numpy as np
+from PIL import Image, ImageDraw
+
+G = 6.67430e-11  # Gravitational constant (m^3/kg/s^2)
+earth_mass = 5.972e24  # Earth's mass (kg)
+
+class SmartOrbitSatellite(Entity):
+    def __init__(self, name, semi_major_axis, semi_minor_axis, eccentricity, inclination, time_factor):
+        super().__init__(
+            model="models/uploads_files_1985975_star+wars.obj",  # Replace with your satellite model
+            scale=(0.025, 0.025, 0.025),
+            # color=color.random_color(),
+        )
         self.name = name
-        self.inclination = inclination
         self.semi_major_axis = semi_major_axis
         self.semi_minor_axis = semi_minor_axis
         self.eccentricity = eccentricity
-        self.orbit_angle = 0  # Initial angle
+        self.inclination = inclination
+        self.orbit_angle = random.uniform(0, 360)  # Initial angle
+        self.time_factor = time_factor  # Store the time factor
+        self.orbital_speed = math.sqrt(G * earth_mass / (semi_major_axis * 1e20))
+        
+        # Create an ellipse to represent the satellite's orbit
+        self.orbit_ellipse = Entity(
+            model='quad',
+            color=color.clear,  # Make the ellipse transparent
+            scale=(semi_major_axis * 2, semi_minor_axis * 2, 1),  # Scale to create an ellipse
+            parent=self,  # Make the ellipse a child of the satellite
+        )
 
-    def set_position(self, angle):
-        self.orbit_angle = angle
-
-    def update_position(self, delta_angle):
-        self.orbit_angle += delta_angle
+    def update(self):
+        self.orbit_angle += self.orbital_speed * time.dt   # Use the stored time factor
+        x = self.semi_major_axis * math.cos(math.radians(self.orbit_angle))
+        z = self.semi_minor_axis * math.sin(math.radians(self.orbit_angle))
+        self.position = (x, 0, z)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from SmartOrbitSatellite import SmartOrbitSatellite
 from ursina import *
 import math
-from SmartOrbitSatellite import SmartOrbitSatellite  # Import the SmartOrbitSatellite class
+import random
 
 app = Ursina()
 
@@ -19,6 +20,7 @@ earth = Entity(
     subdivisions=256,  # Increase subdivisions for smoother appearance
     position=(0, 0, 0),
 )
+
 sun_light = DirectionalLight(color=color.white, y=2, z=-3, shadows=True)
 sun_light.rotation = (10, 30, 30)
 
@@ -27,50 +29,27 @@ sky = Entity(model="sphere", texture=sky_texture, scale=1000, double_sided=True)
 sky.shader = 'unlit'
 
 # Constants
-# orbit_radius = 3  # Radius of the circular orbit
-orbit_speed = 360 / 15  # Degrees per second to complete one orbit in 15 seconds
-orbit_angle = 0  # Initialize orbit angle
+G = 6.67430e-11  # Gravitational constant (m^3/kg/s^2)
+earth_mass = 5.972e24  # Earth's mass (kg)
 
-camera_min_distance = 3
-camera_max_distance = sky.scale_x - 1
-
+# Initial conditions
+satellites = []
+orbital_positions = []
 
 def add_smart_orbit_satellite():
-    global satellites, satellite_entity, orbital_positions
-
-    # Create a new SmartOrbitSatellite instance
-    new_satellite = SmartOrbitSatellite(
-        name="Smart Satellite",
-        inclination=0.0,  # Set inclination angle to 0 for a flat orbit
-        semi_major_axis=8.0,  # Set semi-major axis for an elliptical orbit
-        semi_minor_axis=4.0,  # Set semi-minor axis for an elliptical orbit (half of the major axis)
-        eccentricity=0.0  # Eccentricity should be 0 for a circular orbit
-    )
-
-    # Append the satellite to the list
-    satellites.append(new_satellite)
-
-    # Calculate the initial position on the elliptical orbit
-    satellite_angle = len(satellites) * (360 / len(satellites))  # Equally spaced angles
-    new_satellite.set_position(satellite_angle)
-
-    # Create an Ursina Entity for the satellite
-    satellite_entity = Entity(
-        model="models/uploads_files_1985975_star+wars.obj",  # Replace with your satellite model
-        scale=(0.025, 0.025, 0.025),
-        position=(new_satellite.semi_major_axis * math.cos(math.radians(satellite_angle)),
-                  0,
-                  new_satellite.semi_minor_axis * math.sin(math.radians(satellite_angle)))
-    )
-
-    # Clear the orbital trail
-    orbital_positions = []
-
-    print("Smart orbit satellite added!")
-
+    name = f"Satellite{len(satellites) + 1}"
+    semi_major_axis = random.uniform(2, 20)
+    semi_minor_axis = random.uniform(2, semi_major_axis - 1)
+    eccentricity = math.sqrt(1 - (semi_minor_axis ** 2 / semi_major_axis ** 2))
+    inclination = random.uniform(0, 180)
+    satellite = SmartOrbitSatellite(name, semi_major_axis, semi_minor_axis, eccentricity, inclination, time_factors[current_time_factor_index])
+    satellites.append(satellite)
+    satellite.parent = earth  # Add the satellite as a child of the Earth entity
+    print(f"Smart satellite '{name}' added!")
 
 # Create buttons
 Button(text='Add Dummy Satellite', color=color.gray, position=(-0.7, 0.45), scale=(0.35, 0.05), text_size=5)
+
 # Create a button to add a Smart Orbit Satellite
 smart_button = Button(
     text='Add Smart Orbit Satellite',
@@ -81,12 +60,8 @@ smart_button = Button(
     on_click=add_smart_orbit_satellite
 )
 
-satellites = []
-satellite_entity = None  # Initialize satellite_entity
-
 # Create a list to store orbital trail positions
 orbital_positions = []
-
 
 # Create buttons to control time factor
 time_factors = [1, 5, 10, 100, 1000, 5000, 10000, 20000]
@@ -129,46 +104,13 @@ speed_up_button = Button(
     on_click=speed_up_time
 )
 
+camera_min_distance = 3
+camera_max_distance = sky.scale_x - 1  # You already have this defined
+
 def update():
-    global satellite_entity, orbit_speed, orbit_angle
+    global satellites, orbital_positions
 
     # Rotate the Earth
-    earth.rotation_y -= time.dt * time_factors[current_time_factor_index] * 360/86400  # 360 degrees in 24 hours
-
-    if satellite_entity:
-        # Calculate satellite position in elliptical orbit
-        orbit_angle += orbit_speed * time.dt  # Adjust orbit speed based on time factor
-        semi_major_axis = satellites[0].semi_major_axis  # Convert to meters
-        semi_minor_axis = satellites[0].semi_minor_axis   # Convert to meters
-        distance_from_earth = math.sqrt(
-            (semi_major_axis * math.cos(math.radians(orbit_angle))) ** 2 + (semi_minor_axis * math.sin(
-                math.radians(orbit_angle))) ** 2)
-
-        # Calculate orbital speed based on current distance from Earth
-        gravitational_constant = 6.674 * (10 ** -11)  # Gravitational constant in m^3/kg/s^2
-        earth_mass = 5.972 * (10 ** 24)  # Earth's mass in kg
-        orbital_speed = math.sqrt((gravitational_constant * earth_mass) / distance_from_earth)
-
-        # Update satellite position
-        satellite_x = semi_major_axis * math.cos(math.radians(orbit_angle))
-        satellite_z = semi_minor_axis * math.sin(math.radians(orbit_angle))
-        satellite_entity.position = (satellite_x, 0, satellite_z)
-
-        # Add the current satellite position to the orbital trail
-        orbital_positions.append(satellite_entity.position)
-
-        # Remove old positions to limit the trail length
-        if len(orbital_positions) > 100:
-            orbital_positions.pop(0)
-
-        # Create a Line entity to represent the orbital trail
-        # orbital_line = Line(positions=orbital_positions, color=color.gray, thickness=0.01)
-
-        # Adjust camera distance based on satellite position
-        camera_distance = distance(camera.position, (0, 0, 0))
-        if camera_distance < camera_min_distance:
-            camera.position += (camera.position.normalized() * camera_min_distance - camera.position) * time.dt * 2
-        elif camera_distance > camera_max_distance:
-            camera.position += (camera.position.normalized() * camera_max_distance - camera.position) * time.dt * 2
+    earth.rotation_y -= time.dt * time_factors[current_time_factor_index] * 360 / 86400  # 360 degrees in 24 hours
 
 app.run()
