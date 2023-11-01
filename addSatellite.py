05a6631d42c27c390d525cb1fee6977ee7a86f8d
shobhan dash <28.11.shobhan.dash@gmail.com>
2023-11-01 16:43:38 +0530

    added raan, argp as satellite props

--- addSatellite.py
+++ addSatellite.py
@@ -11,14 +11,13 @@ from world import *
 from timeControl import *
 from timeControl import time_factors, current_time_factor_index, getTimeFactor
 import math
-
+import time
 # Initial conditions
 satellites = []
 orbital_positions = []
 
-
 class SmartOrbitSatellite(Entity):
-    def __init__(self, name, semi_major_axis, semi_minor_axis, inclination, fuel_left, priority):
+    def __init__(self, name, semi_major_axis, semi_minor_axis, inclination, raan, argp, fuel_left, priority):
         super().__init__(
             model="models/satellite_model.obj",
             scale=(0.025, 0.025, 0.025),
@@ -28,75 +27,57 @@ class SmartOrbitSatellite(Entity):
         self.semi_major_axis = semi_major_axis
         self.semi_minor_axis = semi_minor_axis
         self.inclination = inclination
+        self.raan = raan
+        self.argp = argp
         self.fuel_left = fuel_left
         self.priority = priority
 
-        eccentricity = math.sqrt(1 - (semi_minor_axis ** 2 / semi_major_axis ** 2))
-        c = semi_major_axis * math.sqrt(1 - eccentricity**2)
-        # x_foci = c * math.sin(self.inclination)
-        # z_foci = c * math.cos(self.inclination)
-        # Create an orbit using poliastro
+        eccentricity = math.sqrt(1 - ((semi_minor_axis * semi_minor_axis) / (semi_major_axis * semi_major_axis)))
+        # c = semi_major_axis * math.sqrt(1 - eccentricity*eccentricity)
         self.orbit = Orbit.from_classical(
             Earth,
             semi_major_axis * u.km,
             eccentricity * u.one,
             inclination * u.deg,
-            0 * u.deg,
-            0 * u.deg,
+            raan * u.deg,
+            argp * u.deg,
             0 * u.deg,
         )
-
-        self.orbit_circle = Entity(
-            model=Circle(500, mode="line", thickness=1),
-            color=color.gray,
-            scale=(
-                2 * self.semi_major_axis,
-                2 * self.semi_minor_axis,
-            ),
-            shader=lit_with_shadows_shader,
-            parent=universalReferencepoint,  # Make the satellite the parent of the orbit circle
-            rotation=(90, 0, 0),
-            position=(c-semi_major_axis, 0, 0)
-        )
-        self.orbit_circle.rotation_x = -inclination
+        self.recent_positions = []  # Stores the last 100 positions sampled every 50ms
+        self.draw_orbit = Entity(parent=universalReferencepoint, render_queue=1)
 
     def update(self):
-        # Propagate the orbit to the current time
         dt_factor = getTimeFactor() / 1e5
         dt = time.dt * dt_factor
         self.orbit = self.orbit.propagate(dt * u.s)
-        # Update the position of the satellite
         r = (
             self.orbit.r.to(u.km).value / earth.scale_x
-        )  # Convert position to Earth radii
+        )
         self.position = (
             r[0] * earth.scale_x,
             r[1] * earth.scale_y,
             r[2] * earth.scale_z,
         )
-        # Adjust the position and rotation of the orbit circle
-        # self.orbit_circle.position = (x_foci, 0, z_foci)  # Center it at the focus
-        # self.orbit_circle.rotation_z = self.inclination  # Set the inclination
 
+        if len(self.recent_positions) >= 100:
+            self.recent_positions.pop(0)
 
 def add_smart_orbit_satellite_manual():
     global satellites
-    # Create a new tkinter root window
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
 
-    # Create a custom dialog
+    root = tk.Tk()
+    root.withdraw()
+
     dialog = tk.Toplevel(root)
     dialog.title("Enter satellite parameters")
-    dialog.geometry("450x250")
-    # Calculate the screen width and height
+    dialog.geometry("450x350")
+
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x = (screen_width - 400) // 2
     y = (screen_height - 200) // 2
-    dialog.geometry(f"450x250+{x}+{y}")
+    dialog.geometry(f"450x350+{x}+{y}")
 
-    # Create input fields
     name_label = tk.Label(dialog, text="Name:")
     name_entry = tk.Entry(dialog)
     semi_major_axis_label = tk.Label(dialog, text="Semi-major axis (bw [5,15]):")
@@ -105,12 +86,15 @@ def add_smart_orbit_satellite_manual():
     semi_minor_axis_entry = tk.Entry(dialog)
     inclination_label = tk.Label(dialog, text="Inclination (in degrees):")
     inclination_entry = tk.Entry(dialog)
+    raan_label = tk.Label(dialog, text="RAAN (bw [0, 360]):")
+    raan_entry = tk.Entry(dialog)
+    argp_label = tk.Label(dialog, text="ARGP (bw [0, 360]):")
+    argp_entry = tk.Entry(dialog)
     fuel_left_label = tk.Label(dialog, text="Onboard Fuel (bw [0,100] units):")
     fuel_left_entry = tk.Entry(dialog)
     priority_label = tk.Label(dialog, text="Priority (bw [0,5]; Increasing Order):")
     priority_entry = tk.Entry(dialog)
 
-    # Arrange input fields in a grid
     name_label.grid(row=0, column=0)
     name_entry.grid(row=0, column=1)
     semi_major_axis_label.grid(row=1, column=0)
@@ -119,18 +103,23 @@ def add_smart_orbit_satellite_manual():
     semi_minor_axis_entry.grid(row=2, column=1)
     inclination_label.grid(row=3, column=0)
     inclination_entry.grid(row=3, column=1)
-    fuel_left_label.grid(row=4, column=0)
-    fuel_left_entry.grid(row=4, column=1)
-    priority_label.grid(row=5, column=0)
-    priority_entry.grid(row=5, column=1)
+    raan_label.grid(row=4, column=0)
+    raan_entry.grid(row=4, column=1)
+    argp_label.grid(row=5, column=0)
+    argp_entry.grid(row=5, column=1)
+    fuel_left_label.grid(row=6, column=0)
+    fuel_left_entry.grid(row=6, column=1)
+    priority_label.grid(row=7, column=0)
+    priority_entry.grid(row=7, column=1)
 
-    # Create a submit button
     def submit():
         global name, semi_major_axis, semi_minor_axis, inclination, fuel_left, priority
         name = name_entry.get()
         semi_major_axis = float(semi_major_axis_entry.get())
         semi_minor_axis = float(semi_minor_axis_entry.get())
         inclination = float(inclination_entry.get())
+        raan = float(raan_entry.get())
+        argp = float(argp_entry.get())
         fuel_left = float(fuel_left_entry.get())
         priority = int(priority_entry.get())
 
@@ -142,51 +131,45 @@ def add_smart_orbit_satellite_manual():
         dialog.destroy()
 
     def add_smart_orbit_satellite_dummy():
-        # Generate random orbital parameters
         name = f"Satellite{len(satellites) + 1}"
         semi_major_axis = random.uniform(5, 20)
         semi_minor_axis = random.uniform(5,semi_major_axis)
         inclination = random.uniform(0, 180)
+        raan = random.uniform(0, 360)
+        argp  = random.uniform(0, 360)
         fuel_left = random.uniform(0,100)
         priority = random.randint(0,5)
 
-        # Create the satellite with the random parameters
         satellite = SmartOrbitSatellite(
-            name, semi_major_axis, semi_minor_axis, inclination, fuel_left, priority
+            name, semi_major_axis, semi_minor_axis, inclination, raan, argp, fuel_left, priority
         )
         satellite.parent = (
-            universalReferencepoint  # Add the satellite as a child of the Earth entity
+            universalReferencepoint
         )
         satellites.append(satellite)
-        # print(satellites)
         print(
-            f"Smart satellite '{name}' added! (semi-major axis: {semi_major_axis} km, semi-minor axis: {semi_minor_axis}, inclination: {inclination} degrees)"
+            f"Smart satellite '{name}' added! (semi-major axis: {semi_major_axis}, semi-minor axis: {semi_minor_axis}, inclination: {inclination}, RAAN: {raan}, ARGP: {argp}, fuel left: {fuel_left}, priority: {priority})"
         )
         dialog.destroy()
 
     submit_button = tk.Button(dialog, text="Submit", command=submit)
-    submit_button.grid(row=6, column=0, columnspan=2)
+    submit_button.grid(row=8, column=0, columnspan=2)
     or_label = tk.Label(dialog, text="OR")
-    or_label.grid(row=7, column=0, columnspan=2)
+    or_label.grid(row=9, column=0, columnspan=2)
     randomAdd = tk.Button(
         dialog, text="Add Randomly", command=add_smart_orbit_satellite_dummy
     )
-    randomAdd.grid(row=8, column=0, columnspan=2)
+    randomAdd.grid(row=10, column=0, columnspan=2)
 
-    # Wait for the dialog to be destroyed
     root.wait_window(dialog)
 
-    # Check if the dialog was submitted
     if "name" in globals():
-        # Create the satellite with the user's input
         satellite = SmartOrbitSatellite(
             name, semi_major_axis, semi_minor_axis, inclination, fuel_left, priority
         )
         satellite.parent = universalReferencepoint
         satellites.append(satellite)
 
-
-# Create a button to add a Smart Orbit Satellite
 smart_button = Button(
     text="Add Satellite",
     color=color.gray,
@@ -195,7 +178,6 @@ smart_button = Button(
     text_size=5,
     on_click=add_smart_orbit_satellite_manual,
 )
-
 
 def reset_camera():
     camera.position = (0, 0, 0)
